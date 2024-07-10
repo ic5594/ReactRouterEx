a69e374feaef55ec561d995a067098efbe78c220
ic5594 <ic5594@naver.com>
2024-07-10 16:37:48 +0900

    기볼 라우팅 공부

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
+import { createBrowserRouter, 
+  // Route,  
+  RouterProvider,
+  // createRoutesFromElements 
+} from "react-router-dom";
+
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
+import Error from './pages/Error'
+import ProductDetailPage from "./pages/ProductDetail";
+
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage/>}/>
+//   </Route>
+// )
+
+const router = createBrowserRouter([
+  { 
+    path: '/', 
+    element: <RootLayout />,
+    errorElement: <Error/>,
+    children: [
+      { index:true,  element: <HomePage />},
+      { path: 'products', element: <ProductsPage />},
+      { path: 'products/:productId', element: <ProductDetailPage/>}
+    ]
+  }
+])
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router}/>;
 }
 
 export default App;
